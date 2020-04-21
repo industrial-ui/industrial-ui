3247464067c7115e798264c0c7606b83f74924a8
VanishMax <maxkors2014@gmail.com>
2020-04-21 14:44:25 +0900

    Add terser to minify rolllup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
 
 const pkg = require('./package.json');
 
 export default {
   input: 'lib/index.js',
   output: [
-    { file: pkg.module, 'format': 'es' },
+    { file: pkg.module, 'format': 'es', plugins: [terser()] },
     { file: pkg.main, 'format': 'umd', name: 'industrial-ui' },
   ],
   plugins: [
