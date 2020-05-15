f016efaee161697c4258f83ac2888cae4261435f
VanishMax <maxkors2014@gmail.com>
2020-05-15 15:11:02 +0900

    Change all meta tags and update favicon with logos

--- docs/src/routes/docs/[slug].svelte
+++ docs/src/routes/docs/[slug].svelte
@@ -14,7 +14,16 @@
 
 <svelte:head>
   <title>{comp.meta.title || ''} | INDUSTRIAL-UI</title>
-  <meta name="description" content={comp.meta.description || ''}>
+  <meta name="title" content={(comp.meta.title || '') + ' | INDUSTRIAL-UI'}>
+  <meta name="description" content={comp.meta.description || 'Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!'}>
+  <meta name="keywords" content={comp.name + ', industrial-ui, iui, ui-kit, ui-components, library, design, logic, javascript, svelte, front-end, npm'}>
+  <meta name="author" content="VanishMax">
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content={'https://industrial-ui.com/' + comp.name}>
+  <meta property="og:title" content={(comp.meta.title || '') + ' | INDUSTRIAL-UI'}>
+  <meta property="og:description" content={comp.meta.description || 'Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!'}>
+  <meta property="og:image" content="https://industrial-ui.com/img/logo.png">
 </svelte:head>
 
 <svelte:component this={comp[$theme.slug] || comp.component} />
