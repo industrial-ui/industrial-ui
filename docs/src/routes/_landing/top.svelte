892f3939b07a6afcdd803c0012f771009d953e32
VanishMax <maxkors2014@gmail.com>
2020-06-06 18:20:37 +0900

    Add info to header markup

---
+++ docs/src/routes/_landing/top.svelte
@@ -0,0 +1,52 @@
+<script>
+  import WaveIcon from './icons/wave.svelte';
+</script>
+
+<WaveIcon />
+
+<header>
+  <div class="info container">
+    <img src="/img/logo.png" alt="Industrial-ui logo" />
+    <h1>Industrial-ui</h1>
+    <p>Styleless, pure functional UI components</p>
+    <a href="/docs/installation">Go to documentation</a>
+  </div>
+</header>
+
+<style>
+  header {
+    height: 100vh;
+  }
+
+  .info {
+    width: 80%;
+    margin: 10% auto;
+    color: white;
+  }
+
+  img {
+    width: 35px;
+  }
+
+  h1 {
+    font-weight: 500;
+    margin-top: 3.5rem;
+    margin-bottom: 1.3rem;
+    font-size: 3.3em;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 3.5rem;
+    font-size: 1.75em;
+    font-weight: normal;
+  }
+
+  a {
+    display: inline-block;
+    border: 1px solid #FFFFFF;
+    padding: 1rem 2rem;
+    border-radius: 1rem;
+    font-size: 1.2em;
+  }
+</style>
--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -2,19 +2,40 @@
   import Top from './_landing/top.svelte';
 </script>
 
-<Top />
-
 <svelte:head>
-    <title>Industrial-ui – styleless, pure functional UI components</title>
-    <meta name="title" content="Industrial-ui – styleless, pure functional UI components">
-    <meta name="description" content="Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!">
-    <meta name="keywords" content="industrial-ui, iui, ui-kit, ui-components, library, design, logic, javascript, svelte, front-end, npm">
-    <meta name="author" content="VanishMax">
+  <title>Industrial-ui – styleless, pure functional UI components</title>
+  <meta name="title" content="Industrial-ui – styleless, pure functional UI components">
+  <meta name="description" content="Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!">
+  <meta name="keywords" content="industrial-ui, iui, ui-kit, ui-components, library, design, logic, javascript, svelte, front-end, npm">
+  <meta name="author" content="VanishMax">
 
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://industrial-ui.com/">
-    <meta property="og:title" content="Industrial-ui – styleless, pure functional UI components">
-    <meta property="og:description" content="Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!">
-    <meta property="og:image" content="https://industrial-ui.com/img/logo.png">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://industrial-ui.com/">
+  <meta property="og:title" content="Industrial-ui – styleless, pure functional UI components">
+  <meta property="og:description" content="Production-oriented library with a feature rich UI components with no styles at all. Enjoy coding!">
+  <meta property="og:image" content="https://industrial-ui.com/img/logo.png">
+
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/style/landing.css" >
 </svelte:head>
 
+<main>
+  <Top />
+
+  <section class="introduction">
+  </section>
+
+  <section class="steps">
+  </section>
+
+  <section class="users">
+  </section>
+
+
+  <footer>
+  </footer>
+</main>
+
+<style>
+
+</style>
