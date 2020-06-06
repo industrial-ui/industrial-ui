8da77c8330b890c5c516c31f4e8ffe9846d50b00
VanishMax <maxkors2014@gmail.com>
2020-06-06 19:53:34 +0900

    Make wave icon responsive

--- docs/src/routes/_landing/top.svelte
+++ docs/src/routes/_landing/top.svelte
@@ -1,12 +1,17 @@
 <script>
-  import WaveIcon from './icons/wave.svelte';
 </script>
 
-<WaveIcon />
+<img
+  class="wave"
+  srcset="/img/wave-mobile.svg 480w, /img/wave.svg 1600w"
+  sizes="(max-width: 600px) 100vw, 100vw"
+  src="/img/wave.svg"
+  alt="Elva dressed as a fairy"
+>
 
 <header>
   <div class="info container">
-    <img src="/img/logo.png" alt="Industrial-ui logo" />
+    <img class="logo" src="/img/logo.png" alt="Industrial-ui logo" />
     <h1>Industrial-ui</h1>
     <p>Styleless, pure functional UI components</p>
     <a href="/docs/installation">Go to documentation</a>
@@ -24,8 +29,14 @@
     color: white;
   }
 
-  img {
-    width: 35px;
+  .logo {
+    width: 2.75rem;
+  }
+
+  .wave {
+    position: absolute;
+    z-index: -1;
+    width: 100%;
   }
 
   h1 {
@@ -48,5 +59,27 @@
     padding: 1rem 2rem;
     border-radius: 1rem;
     font-size: 1.2em;
+  }
+
+  @media screen and (max-width: 479px) {
+    .info {
+      width: 220px;
+      margin-top: 10%;
+      margin-left: 20%;
+    }
+
+    .wave {
+      width: 150%;
+    }
+
+    h1 {
+      /*font-size: 3em;*/
+      margin-top: 1.75rem;
+      margin-bottom: 1rem;
+    }
+
+    p {
+      margin-bottom: 1.75rem;
+    }
   }
 </style>
